bd84771ec18ea413536f5c9fbd1bec9fc7b27bf1
Mathieu Hofman <86499+mhofman@users.noreply.github.com>
2018-01-11 17:15:36 -0800

    Iterate over tabs in each windows

--- source/background.js
+++ source/background.js
@@ -1,20 +1,20 @@
-(function(tabs){
+(function(tabs, windows){
 
     "use strict";
 
     const specialUrls =/chrome-extension:|chrome:|chrome-devtools:|file:|chrome.google.com\/webstore/;
 
     let discardAllTabs = () => {
-        tabs.query({}, tabsList => {
-            tabsList.forEach( tab => {
+        windows.getAll({populate: true}, windowsList => windowsList.forEach( win => {
+            win.tabs.forEach( tab => {
                 if(tab.pinned || tab.active || tab.discarded) { return; }
                 if(tab.url && specialUrls.test(tab.url)){ return; }
                 tabs.discard(tab.id);
             } )
-        });
+        } ));
     };
 
     chrome.browserAction.onClicked.addListener( discardAllTabs );
     chrome.runtime.onStartup.addListener( discardAllTabs );
 
-})(chrome.tabs);
+})(chrome.tabs, chrome.windows);
