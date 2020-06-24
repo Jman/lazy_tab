5ef0d495eee968fbcfc142eae75d6857700ecd9f
Eugene Tereshchenko <i@jman.info>
2020-06-24 17:16:49 +0300

    remove url checks and tabs permission

--- source/background.js
+++ source/background.js
@@ -2,19 +2,18 @@
 
     "use strict";
 
-    const specialUrls =/chrome-extension:|chrome:|chrome-devtools:|file:|chrome.google.com\/webstore/;
-
-    let discardAllTabs = () => {
-        windows.getAll({populate: true}, windowsList => windowsList.forEach( win => {
+    const discardAllTabs = () => {
+        windows.getAll({ populate: true }, windowsList => windowsList.forEach( win => {
             const minimized = win.state === 'minimized';
 
             win.tabs.forEach( tab => {
                 if(tab.discarded) { return; }
                 if(tab.pinned) { return; }
                 if(tab.active && !minimized) { return; }
-                if(tab.url && specialUrls.test(tab.url)){ return; }
-                tabs.discard(tab.id);
-            } )
+                try{
+                    tabs.discard(tab.id);
+                } catch (e) {}
+            } );
         } ));
     };
 
